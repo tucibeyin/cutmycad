ca2662f16f781b8d9d4d1abf63ef031645b3fab2
tucibeyin <tucibeyin@hotmail.com>
2025-12-12 12:31:40 +0300

    home

---
+++ assets/js/views/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return `
+        <section>
+            <h1>Hoş Geldiniz</h1>
+            <p>CutMyCad sistemine giriş yaparak işlemlerinize devam edebilirsiniz.</p>
+        </section>
+    `;
+}
